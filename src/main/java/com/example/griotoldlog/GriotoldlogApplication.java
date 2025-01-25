f115cd5d03a53c782b655263370fbe5af0254b10
Griotold <gotjd9773@naver.com>
2025-01-25 11:58:26 +0900

    Initial commit

---
+++ src/main/java/com/example/griotoldlog/GriotoldlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.griotoldlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GriotoldlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GriotoldlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/griotoldlog/GriotoldlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.griotoldlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GriotoldlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
